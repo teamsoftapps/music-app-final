d2e678dc78734fad21ccf87257762eedf7596786
Ameen Ilyas <=>
2021-10-16 21:38:10 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -4,22 +4,26 @@ module.exports = (phase) => {
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
             reactStrictMode: true,
-            images: { domains: ["localhost:5000", "localhost", "18.119.12.204"] },
+            images: { domains: ["localhost:5000", "localhost", "3.144.44.95"] },
             env: {
-                base_url: "http://18.119.12.204/api",
-                media_url: "http://18.119.12.204",
+                base_url: "http://3.144.44.95/api",
+                media_url: "http://3.144.44.95",
+            },
+            httpAgentOptions: {
+                keepAlive: true,
             },
         };
     }
 
     return {
         reactStrictMode: true,
-        images: { domains: ["music-appps.herokuapp.com", "18.119.12.204"] },
+        images: { domains: ["music-appps.herokuapp.com", "3.144.44.95"] },
         env: {
-            base_url: "http://18.119.12.204/api",
-            media_url: "http://18.119.12.204",
-            // base_url: "https://music-appps.herokuapp.com/api",
-            // media_url: "https://music-appps.herokuapp.com",
+            base_url: "https://music-appps.herokuapp.com/api",
+            media_url: "https://music-appps.herokuapp.com",
+            httpAgentOptions: {
+                keepAlive: true,
+            },
         },
     };
 };
