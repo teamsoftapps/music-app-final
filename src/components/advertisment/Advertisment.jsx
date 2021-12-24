73d0a61871a0f494a54008f579678f4bb5ed7775
miller-music <96537063+miller-music@users.noreply.github.com>
2021-12-24 12:18:06 -0500

    Undo adding of update

--- src/components/advertisment/Advertisment.jsx
+++ src/components/advertisment/Advertisment.jsx
@@ -1,10 +1,6 @@
 const Advertisement = () => {
     return (
         <div style={{ height: "100%" }}>
-<div style="padding:5px;color:#fff;vertical-align: middle;display: table;">
-<iframe src="https://player.vimeo.com/video/659963617?h=d0cd07c6bd" height="150" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" style=" float: left; "></iframe>
-<span style="display:table-cell;vertical-align: middle;">Mulder plays Christmas organ improvisation during (pre-COVID) European Tour.</span>
-</div>
             {/* <h2 style={{ color: "white", textAlign: "center" }}></h2> */}
             {/* <video>
                 <source src="" type="video/mp4" />
