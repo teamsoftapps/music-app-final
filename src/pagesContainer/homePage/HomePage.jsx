dde697e284f3e95d086be86d25a880312dea8af1
miller-music <96537063+miller-music@users.noreply.github.com>
2021-12-24 14:10:08 -0500

    Update HomePage.jsx

--- src/pagesContainer/homePage/HomePage.jsx
+++ src/pagesContainer/homePage/HomePage.jsx
@@ -34,7 +34,7 @@ const HomePage = ({ albums }) => {
             {/* Code for Advertisement (start) */}
             <div className={classes.addcontainer}>
                 <h3 className={classes.addheading} onClick={() => handleAdd()}>
-                    Exclusive Content
+                    Exclusive Video
                 </h3>
 
                 {openAdd === false ? (
