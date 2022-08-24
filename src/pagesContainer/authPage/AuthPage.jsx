36ca75e5b170f876955a54697ff583a2ec730632
teamsofsoftapps <softapps.io2@gmail.com>
2022-08-24 18:44:56 +0500

    fix issue on home page, auth page and advertisement

--- src/pagesContainer/authPage/AuthPage.jsx
+++ src/pagesContainer/authPage/AuthPage.jsx
@@ -10,235 +10,281 @@ import Head from "next/head";
 
 const postSelector = (state) => state.music;
 function AuthPage({ isSignIn }) {
-    const { language } = useSelector(postSelector, shallowEqual);
+  const { language } = useSelector(postSelector, shallowEqual);
 
-    const router = useRouter();
-    const dispatch = useDispatch();
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-    const [accessCode, setAccessCode] = useState("");
-    const [checkBox, setCheckBox] = useState(false);
-    const [isForget, setIsForget] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [accessCode, setAccessCode] = useState("");
+  const [checkBox, setCheckBox] = useState(false);
+  const [isForget, setIsForget] = useState(false);
 
-    // console.log({ email, accessCode });
-    // console.log(router.query.email ? router.query.email : "", router.query.access_code ? router.query.access_code : "");
+  // console.log({ email, accessCode });
+  // console.log(router.query.email ? router.query.email : "", router.query.access_code ? router.query.access_code : "");
 
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("music-app-credentials"));
-        if (user?.token.length > 30) {
-            router.replace("/");
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("music-app-credentials"));
+
+    if (user?.token.length > 30) router.replace("/");
+
+    let pageName = router.asPath.slice(1, 7);
+    if (pageName === "signup") {
+      try {
+        let query = router.asPath.slice(8).split("&");
+        let subQuery = [];
+        for (let i in query) {
+          subQuery.push(query[i].split("="));
         }
-        let pageName = router.asPath.slice(1, 7);
-        if (pageName === "signup") {
-            try {
-                let query = router.asPath.slice(8).split("&");
-                let subQuery = [];
-                for (let i in query) {
-                    subQuery.push(query[i].split("="));
-                }
-                let access_code = findParam("access_code", subQuery);
-                let email = findParam("email", subQuery);
-                setEmail(email);
-                setAccessCode(access_code);
-            } catch (e) {
-                // Do nothing
-                console.log(e);
-            }
-        }
-    }, []);
-
-    const findParam = (name, array) => {
-        for (let i in array) {
-            if (array[i][0] === name) {
-                return array[i][1];
-            }
-        }
-        return "";
-    };
-
-    async function handleSubmit(e) {
-        setLoading(true);
-        e.preventDefault();
-        const payload = isSignIn && !isForget ? { email, password, code: accessCode } : { email, password, code: accessCode };
-
-        const url = process.env.base_url + (!isSignIn ? "/signup" : "/signin");
-
-        try {
-            const { data } = await axios.post(url, payload);
-            console.log(data);
-            setLoading(false);
-            localStorage.setItem("music-app-credentials", JSON.stringify(data));
-            dispatch(setUser(data));
-
-            router.push("/");
-        } catch (err) {
-            setLoading(false);
-            console.log({ err });
-            setError(err?.response?.data);
-
-            setTimeout(() => {
-                setError("");
-            }, 3000);
-        }
+        let access_code = findParam("access_code", subQuery);
+        let email = findParam("email", subQuery);
+        setEmail(email);
+        setAccessCode(access_code);
+      } catch (e) {
+        // Do nothing
+        console.log(e);
+      }
     }
+  }, []);
 
-    const loginTextEng = isSignIn ? "Login" : "Sign Up";
-    const loginTextNl = isSignIn ? "Inloggen" : "Maak uw account aan";
-    const codePromiseText =
-        language.title === "nl"
-            ? "Ik ben de exclusieve gebruiker van deze account en beloof de muziek niet te delen met derden."
-            : "I promise this account will only be used by me, and not to share any of the content with others.";
+  const findParam = (name, array) => {
+    for (let i in array) {
+      if (array[i][0] === name) {
+        return array[i][1];
+      }
+    }
+    return "";
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className={classes.auth}>
-            <Head>
-                <title>Mulder Music Streaming | {language.title === "nl" ? loginTextNl : loginTextEng} </title>
-            </Head>
+  async function handleSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+    const payload =
+      isSignIn && !isForget
+        ? { email, password, code: accessCode }
+        : { email, password, code: accessCode };
 
-            <h1>{isForget ? "Reset" : language.title === "nl" ? loginTextNl : loginTextEng}</h1>
-            {loading && <h3>Loading..</h3>}
-            {error && <h3 style={{ color: "red" }}>{error}</h3>}
+    const url = process.env.base_url + (!isSignIn ? "/signup" : "/signin");
 
-            <div className={classes.input}>
-                <label htmlFor="">Email</label>
-                <input
-                    value={email}
-                    onChange={
-                        isSignIn
-                            ? (e) => {
-                                  setEmail(e.target.value);
-                              }
-                            : (e) => {
-                                  setEmail(e.target.value);
-                              }
-                    }
-                    // disabled={!isSignIn ? true : false}
-                    type="email"
-                    required
-                    placeholder={language.title === "nl" ? "Uw emailadres" : "Your Email Address"}
-                />
-            </div>
-            {/* Email */}
-            {/* Disable conditions on the basis of /signup and /signup?uduiwe */}
+    try {
+      const { data } = await axios.post(url, payload);
+      console.log(data);
+      setLoading(false);
+      localStorage.setItem("music-app-credentials", JSON.stringify(data));
+      dispatch(setUser(data));
 
-            <div className={classes.input}>
-                <label htmlFor="">
-                    {isForget ? (language.title === "nl" ? "Nieuw" : "New") : ""} {language.title === "nl" ? "Wachtwoord" : "Password"}
-                </label>
-                <input
-                    value={password}
-                    type="password"
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                    required
-                    minLength={6}
-                    maxLength={36}
-                    placeholder={
-                        isForget
-                            ? language.title === "nl"
-                                ? "Nieuw Wachtwoord"
-                                : "Your New Password"
-                            : language.title === "nl"
-                            ? "Wachtwoord"
-                            : "Your Password"
-                    }
-                />
-            </div>
-            {!isSignIn || isForget ? (
-                <div className={classes.input}>
-                    <label htmlFor="">{language.title === "nl" ? "Toegangscode" : "Access Code"}</label>
-                    <input
-                        value={accessCode}
-                        // disabled={!isSignIn ? true : false}
-                        onChange={
-                            isSignIn
-                                ? (e) => {
-                                      setAccessCode(e.target.value);
-                                  }
-                                : (e) => {
-                                      setAccessCode(e.target.value);
-                                  }
-                        }
-                        type="text"
-                        required
-                        minLength={7}
-                        maxLength={10}
-                        placeholder={language.title === "nl" ? "Toegangscode" : "Access Code"}
-                    />
-                </div>
-            ) : (
-                ""
-            )}
-            {!isSignIn && (
-                <>
-                    <br />
-                    <FormControlLabel
-                        style={{ display: "block" }}
-                        control={<Checkbox color="default" value={checkBox} onClick={() => setCheckBox(!checkBox)} />}
-                        label={codePromiseText}
-                    />
-                </>
-            )}
-            <Button disabled={!isSignIn && !checkBox} type="submit" variant="contained">
-                {isForget && language.title === "nl" ? "Resetten" : isForget && language.title === "eng" ? "Reset" : loginTextEng}
-            </Button>
-            <br />
-            <p>
-                {!isSignIn ? (
-                    <span onClick={() => router.push("/login")}>
-                        {language.title === "nl" ? "Heeft u al een account? Inloggen." : "Already have an account"}
-                    </span>
-                ) : (
-                    <>
-                        <span className={classes.linkBoxWrapper}>
-                            {isForget ? (
-                                <span
-                                    onClick={() => {
-                                        setIsForget(!isForget);
-                                        setAccessCode("");
-                                        setPassword("");
-                                    }}
-                                >
-                                    {language.title === "nl" ? "Terug naar Inloggen" : "Back to Login"}
-                                </span>
-                            ) : (
-                                <span
-                                    onClick={() => {
-                                        setIsForget(!isForget);
-                                        setPassword("");
-                                    }}
-                                >
-                                    {language.title === "nl" ? "Wachtwoord vergeten?" : "Forgot Password?"}
-                                </span>
-                            )}
-                            {/* <span onClick={() => router.push("/signup")}>
+      router.push("/");
+    } catch (err) {
+      setLoading(false);
+      console.log({ err });
+      setError(err?.response?.data);
+
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  }
+
+  const loginTextEng = isSignIn ? "Login" : "Sign Up";
+  const loginTextNl = isSignIn ? "Inloggen" : "Maak uw account aan";
+  const codePromiseText =
+    language.title === "nl"
+      ? "Ik ben de exclusieve gebruiker van deze account en beloof de muziek niet te delen met derden."
+      : "I promise this account will only be used by me, and not to share any of the content with others.";
+
+  return (
+    <form onSubmit={handleSubmit} className={classes.auth}>
+      <Head>
+        <title>
+          Mulder Music Streaming |{" "}
+          {language.title === "nl" ? loginTextNl : loginTextEng}{" "}
+        </title>
+      </Head>
+
+      <h1>
+        {isForget
+          ? "Reset"
+          : language.title === "nl"
+          ? loginTextNl
+          : loginTextEng}
+      </h1>
+      {loading && <h3>Loading..</h3>}
+      {error && <h3 style={{ color: "red" }}>{error}</h3>}
+
+      <div className={classes.input}>
+        <label htmlFor="">Email</label>
+        <input
+          value={email}
+          onChange={
+            isSignIn
+              ? (e) => {
+                  setEmail(e.target.value);
+                }
+              : (e) => {
+                  setEmail(e.target.value);
+                }
+          }
+          // disabled={!isSignIn ? true : false}
+          type="email"
+          required
+          placeholder={
+            language.title === "nl" ? "Uw emailadres" : "Your Email Address"
+          }
+        />
+      </div>
+      {/* Email */}
+      {/* Disable conditions on the basis of /signup and /signup?uduiwe */}
+
+      <div className={classes.input}>
+        <label htmlFor="">
+          {isForget ? (language.title === "nl" ? "Nieuw" : "New") : ""}{" "}
+          {language.title === "nl" ? "Wachtwoord" : "Password"}
+        </label>
+        <input
+          value={password}
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+          minLength={6}
+          maxLength={36}
+          placeholder={
+            isForget
+              ? language.title === "nl"
+                ? "Nieuw Wachtwoord"
+                : "Your New Password"
+              : language.title === "nl"
+              ? "Wachtwoord"
+              : "Your Password"
+          }
+        />
+      </div>
+      {!isSignIn || isForget ? (
+        <div className={classes.input}>
+          <label htmlFor="">
+            {language.title === "nl" ? "Toegangscode" : "Access Code"}
+          </label>
+          <input
+            value={accessCode}
+            // disabled={!isSignIn ? true : false}
+            onChange={
+              isSignIn
+                ? (e) => {
+                    setAccessCode(e.target.value);
+                  }
+                : (e) => {
+                    setAccessCode(e.target.value);
+                  }
+            }
+            type="text"
+            required
+            minLength={7}
+            maxLength={10}
+            placeholder={
+              language.title === "nl" ? "Toegangscode" : "Access Code"
+            }
+          />
+        </div>
+      ) : (
+        ""
+      )}
+      {!isSignIn && (
+        <>
+          <br />
+          <FormControlLabel
+            style={{ display: "block" }}
+            control={
+              <Checkbox
+                color="default"
+                value={checkBox}
+                onClick={() => setCheckBox(!checkBox)}
+              />
+            }
+            label={codePromiseText}
+          />
+        </>
+      )}
+      <Button
+        disabled={!isSignIn && !checkBox}
+        type="submit"
+        variant="contained"
+      >
+        {isForget && language.title === "nl"
+          ? "Resetten"
+          : isForget && language.title === "eng"
+          ? "Reset"
+          : loginTextEng}
+      </Button>
+      <br />
+      <p>
+        {!isSignIn ? (
+          <span onClick={() => router.push("/login")}>
+            {language.title === "nl"
+              ? "Heeft u al een account? Inloggen."
+              : "Already have an account"}
+          </span>
+        ) : (
+          <>
+            <span className={classes.linkBoxWrapper}>
+              {isForget ? (
+                <span
+                  onClick={() => {
+                    setIsForget(!isForget);
+                    setAccessCode("");
+                    setPassword("");
+                  }}
+                >
+                  {language.title === "nl"
+                    ? "Terug naar Inloggen"
+                    : "Back to Login"}
+                </span>
+              ) : (
+                <span
+                  onClick={() => {
+                    setIsForget(!isForget);
+                    setPassword("");
+                  }}
+                >
+                  {language.title === "nl"
+                    ? "Wachtwoord vergeten?"
+                    : "Forgot Password?"}
+                </span>
+              )}
+              {/* <span onClick={() => router.push("/signup")}>
                                 {language.title === "nl" ? "Account aanmaken" : "Create Your Account"}
                             </span> */}
-                        </span>
-                    </>
-                )}
-            </p>
-            {!isSignIn && (
-                <>
-                    <br />
-                    {/* <span>Forgot your password</span> */}
-                    {language.title === "nl" ? (
-                        <a target="_blank" href="https://janmulder.us/store/?album=Streaming">
-                            Door u aan te melden gaat u akkoord met onze terms & conditions.
-                        </a>
-                    ) : (
-                        <a target="_blank" href="https://janmulder.us/store/?album=Streaming">
-                            By Signing up, you are agree to follow our <span>Terms & conditions.</span>
-                        </a>
-                    )}
-                </>
-            )}
-        </form>
-    );
+            </span>
+          </>
+        )}
+      </p>
+      {!isSignIn && (
+        <>
+          <br />
+          {/* <span>Forgot your password</span> */}
+          {language.title === "nl" ? (
+            <a
+              target="_blank"
+              href="https://janmulder.us/store/?album=Streaming"
+            >
+              Door u aan te melden gaat u akkoord met onze terms & conditions.
+            </a>
+          ) : (
+            <a
+              target="_blank"
+              href="https://janmulder.us/store/?album=Streaming"
+            >
+              By Signing up, you are agree to follow our{" "}
+              <span>Terms & conditions.</span>
+            </a>
+          )}
+        </>
+      )}
+    </form>
+  );
 }
 
 export default AuthPage;
--- src/pagesContainer/homePage/HomePage.jsx
+++ src/pagesContainer/homePage/HomePage.jsx
@@ -12,104 +12,114 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { setFavourites } from "../../store/musicReducer";
 
-
 const postSelector = (state) => state.music;
 
 const HomePage = ({ albums }) => {
+  let albumArray = [];
 
+  albums?.forEach((element) => {
+    let { index } = element;
+    albumArray[index - 1] = element;
+  });
 
-    let albumArray = [];
+  // console.log(albumArray)
+  const [openAdd, setOpenAdd] = useState(false);
+  const { language, user } = useSelector(postSelector, shallowEqual);
+  const route = useRouter();
+  const dispatch = useDispatch();
+  // console.log(user)
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("music-app-credentials"));
 
-    albums?.forEach(element => {
-        let { index } = element;
-        albumArray[index - 1] = element;
-    });
+    if (!user) return route.replace("/login");
 
+    const { token } = JSON.parse(localStorage.getItem("music-app-credentials"));
 
-    // console.log(albumArray)
-    const [openAdd, setOpenAdd] = useState(false);
-    const { language, user } = useSelector(postSelector, shallowEqual);
-    const route = useRouter()
-    const dispatch = useDispatch()
-    // console.log(user)
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("music-app-credentials"));
-
-        if (!user?.token.length) return route.replace("/login");
-
-        const { token } = JSON.parse(localStorage.getItem('music-app-credentials'))
-
-        const fetchFavourites = async () => {
-            try {
-                const url = process.env.base_url;
-                const { data } = await axios.get(`${url}/getFavourites`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-                // tempArr.push(data?.favourites)
-                dispatch(setFavourites(data?.favourites))
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchFavourites()
-
-    }, []);
-
-    const handleAdd = () => {
-        if (openAdd === false) {
-            setOpenAdd(true);
-        } else {
-            setOpenAdd(false);
-        }
-        console.log(openAdd);
+    const fetchFavourites = async () => {
+      try {
+        const url = process.env.base_url;
+        const { data } = await axios.get(`${url}/getFavourites`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        // tempArr.push(data?.favourites)
+        dispatch(setFavourites(data?.favourites));
+      } catch (error) {
+        console.log(error);
+      }
     };
+    fetchFavourites();
+  }, []);
 
-    return (
-        <div className={classes.homePage}>
-            <br />
-            <h4 style={{ color: "white", textAlign: "center" }}>STREAMING</h4>
-            {/* Code for Advertisement (start) */}
-            <div className={classes.addcontainer}>
-                <h3 className={classes.addheading} onClick={() => handleAdd()}>
-                    Exclusive Video
-                </h3>
+  const handleAdd = () => {
+    if (openAdd === false) {
+      setOpenAdd(true);
+    } else {
+      setOpenAdd(false);
+    }
+    console.log(openAdd);
+  };
 
-                {openAdd === false ? (
-                    <Image src={downarrow} width={16} height={16} className={classes.arrows} onClick={() => handleAdd()} />
-                ) : (
-                    <Image src={uparrow} width={16} height={16} className={classes.arrows} onClick={() => handleAdd()} />
-                )}
-                {openAdd === true && (
-                    <div className={classes.addwrapper}>
-                        <Advertisement />
-                    </div>
-                )}
-            </div>
+  return (
+    <div className={classes.homePage}>
+      <br />
+      <h4 style={{ color: "white", textAlign: "center" }}>STREAMING</h4>
+      {/* Code for Advertisement (start) */}
+      <div className={classes.addcontainer}>
+        <h3 className={classes.addheading} onClick={() => handleAdd()}>
+          Exclusive Video
+        </h3>
 
-            {/* Code for Advertisement (end) */}
-            <FlipMove className={classes.cards}>
-                {albumArray?.map((album, index) => {
-                    const url = `${process.env.media_url}/${language.title === "eng" ? album?.Album_Image : album?.Album_Image.replace("eng", "nl")
-                        }`;
+        {openAdd === false ? (
+          <Image
+            src={downarrow}
+            width={16}
+            height={16}
+            className={classes.arrows}
+            onClick={() => handleAdd()}
+          />
+        ) : (
+          <Image
+            src={uparrow}
+            width={16}
+            height={16}
+            className={classes.arrows}
+            onClick={() => handleAdd()}
+          />
+        )}
+        {openAdd === true && (
+          <div className={classes.addwrapper}>
+            <Advertisement />
+          </div>
+        )}
+      </div>
 
-                    return (
-                        <Card
-                            key={album?._id + language.title}
-                            album={album}
-                            url={url}
-                            index={index}
-                            trial={user?.hasOwnProperty("expiresIn")}
-                        />
-                    );
-                })}
-            </FlipMove>
-            {/* https://githubmemory.com/repo/joshwcomeau/react-flip-move/issues/256 */}
-            {/* Using UNSAFE_componentWillReceiveProps in strict mode is not recommended and may indicate bugs in your code. */}
-            <Footer />
-        </div>
-    );
+      {/* Code for Advertisement (end) */}
+      <FlipMove className={classes.cards}>
+        {albumArray?.map((album, index) => {
+          const url = `${process.env.media_url}/${
+            language.title === "eng"
+              ? album?.Album_Image
+              : album?.Album_Image.replace("eng", "nl")
+          }`;
+
+          return (
+            <Card
+              key={album?._id + language.title}
+              album={album}
+              url={url}
+              index={index}
+              trial={user?.hasOwnProperty("expiresIn")}
+            />
+          );
+        })}
+      </FlipMove>
+      {/* https://githubmemory.com/repo/joshwcomeau/react-flip-move/issues/256 */}
+      {/* Using UNSAFE_componentWillReceiveProps in strict mode is not recommended and may indicate bugs in your code. */}
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
