bd8592883c59854a22527178786149d154d96f56
SoftApps.io <66966682+teamsoftapps@users.noreply.github.com>
2022-08-25 16:14:19 +0500

    handleLike called in useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import HomePage from "../src/pagesContainer/homePage/HomePage";
 import axios from "axios";
+// import Loader from "react-loader-spinner";
 
 const index = ({ albums }) => <HomePage albums={albums} />;
+// const [isLoading, setLoading] = useState(false);
 
 export default index;
 
 export async function getStaticProps() {
+  // setLoading(true);
   const { data } = await axios.get(`${process.env.base_url}/albums`);
 
   return {
     props: {
       albums: data,
-      revalidate: 1800,
+      revalidate: 20,
     },
   };
 }
