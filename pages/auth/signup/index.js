f8bb24f4650b0e7f0f97c910094ed3ea7001c76e
teamsofsoftapps <softapps.io2@gmail.com>
2022-08-26 12:42:24 +0500

    add login and signup file

---
+++ pages/auth/signup/index.js
@@ -0,0 +1,11 @@
+import SignupPage from "../../../src/pagesContainer/signupPage";
+import Footer from "../../../src/components/footer";
+
+const index = () => (
+  <>
+    <SignupPage />
+    <Footer />
+  </>
+);
+
+export default index;
