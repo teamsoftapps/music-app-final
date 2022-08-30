9ee28a20de5450958734956b542209cfa9250e98
teamsofsoftapps <softapps.io2@gmail.com>
2022-08-30 15:13:39 +0500

    update forgot password ui

--- src/pagesContainer/forgotPage/index.jsx
+++ src/pagesContainer/forgotPage/index.jsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import classes from "./ForgotPage.module.css";
-import api from "./../../../services/api";
 
 const postSelector = (state) => state.music;
 
@@ -19,15 +18,11 @@ const ForgotPage = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [isForget, setIsForget] = useState(false);
-
-  // console.log({ email, accessCode });
-  // console.log(router.query.email ? router.query.email : "", router.query.access_code ? router.query.access_code : "");
 
   const handleSubmit = async (e) => {
-    // setLoading(true);
-    // e.preventDefault();
-    // const payload = { email };
+    setLoading(true);
+    e.preventDefault();
+    const payload = { email };
   };
 
   return (
@@ -51,7 +46,7 @@ const ForgotPage = () => {
           onChange={(e) => {
             setEmail(e.target.value);
           }}
-          // disabled={!isSignIn ? true : false}
+          // disabled={loading ? true : false}
           type="email"
           required
           placeholder={
@@ -60,151 +55,23 @@ const ForgotPage = () => {
         />
       </div>
 
-      {/* Email */}
-      {/* Disable conditions on the basis of /signup and /signup?uduiwe */}
-
-      {/* <div className={classes.input}>
-        <label htmlFor="">
-          {isForget ? (language.title === "nl" ? "Nieuw" : "New") : ""}{" "}
-          {language.title === "nl" ? "Wachtwoord" : "Password"}
-        </label>
-        <input
-          value={password}
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          required
-          minLength={6}
-          maxLength={36}
-          placeholder={
-            isForget
-              ? language.title === "nl"
-                ? "Nieuw Wachtwoord"
-                : "Your New Password"
-              : language.title === "nl"
-              ? "Wachtwoord"
-              : "Your Password"
-          }
-        />
-      </div> */}
-
-      {/* {!isSignIn || isForget ? (
-        <div className={classes.input}>
-          <label htmlFor="">
-            {language.title === "nl" ? "Toegangscode" : "Access Code"}
-          </label>
-          <input
-            value={accessCode}
-            // disabled={!isSignIn ? true : false}
-            onChange={
-              isSignIn
-                ? (e) => {
-                    setAccessCode(e.target.value);
-                  }
-                : (e) => {
-                    setAccessCode(e.target.value);
-                  }
-            }
-            type="text"
-            required
-            minLength={7}
-            maxLength={10}
-            placeholder={
-              language.title === "nl" ? "Toegangscode" : "Access Code"
-            }
-          />
-        </div>
-      ) : (
-        ""
-      )} */}
-      {/* {!isSignIn && (
-        <>
-          <br />
-          <FormControlLabel
-            style={{ display: "block" }}
-            control={
-              <Checkbox
-                color="default"
-                value={checkBox}
-                onClick={() => setCheckBox(!checkBox)}
-              />
-            }
-            label={codePromiseText}
-          />
-        </>
-      )} */}
-      <Button
-        // disabled={!isSignIn && !checkBox}
-        type="submit"
-        variant="contained"
-      >
+      <Button type="submit" variant="contained">
         {language.title === "nl" ? "Indienen" : "Submit"}
       </Button>
+
       <br />
-      {/* <p>
-        {!isSignIn ? (
-          <span onClick={() => router.push("/auth/login")}>
-            {language.title === "nl"
-              ? "Heeft u al een account? Inloggen."
-              : "Already have an account"}
+
+      <p>
+        <span className={classes.linkBoxWrapper}>
+          <span
+            onClick={() => {
+              router.push("/auth/login");
+            }}
+          >
+            {language.title === "nl" ? "Terug naar Inloggen" : "Back to Login"}
           </span>
-        ) : (
-          <>
-            <span className={classes.linkBoxWrapper}>
-              {isForget ? (
-                <span
-                  onClick={() => {
-                    setIsForget(!isForget);
-                    // setAccessCode("");
-                    // setPassword("");
-                  }}
-                >
-                  {language.title === "nl"
-                    ? "Terug naar Inloggen"
-                    : "Back to Login"}
-                </span>
-              ) : (
-                <span
-                  onClick={() => {
-                    setIsForget(!isForget);
-                    setPassword("");
-                  }}
-                >
-                  {language.title === "nl"
-                    ? "Wachtwoord vergeten?"
-                    : "Forgot Password?"}
-                </span>
-              )} */}
-      {/* <span onClick={() => router.push("/signup")}>
-                                {language.title === "nl" ? "Account aanmaken" : "Create Your Account"}
-                            </span> */}
-      {/* </span>
-          </>
-        )}
-      </p> */}
-      {/* {!isSignIn && (
-        <>
-          <br /> */}
-      {/* <span>Forgot your password</span> */}
-      {/* {language.title === "nl" ? (
-            <a
-              target="_blank"
-              href="https://janmulder.us/store/?album=Streaming"
-            >
-              Door u aan te melden gaat u akkoord met onze terms & conditions.
-            </a>
-          ) : (
-            <a
-              target="_blank"
-              href="https://janmulder.us/store/?album=Streaming"
-            >
-              By Signing up, you are agree to follow our{" "}
-              <span>Terms & conditions.</span>
-            </a>
-          )}
-        </>
-      )} */}
+        </span>
+      </p>
     </form>
   );
 };
