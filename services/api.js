9b83f57fbc6a98108792da7c18d7f3bbc451b611
teamsoftapps <softapps.io2@gmail.com>
2022-12-06 15:39:26 +0500

    add log

--- services/api.js
+++ services/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+console.log({
+  base_url: process.env.base_url,
+  media_url: process.env.media_url,
+});
+
 const api = axios.create({
   baseURL: process.env.base_url,
 });
