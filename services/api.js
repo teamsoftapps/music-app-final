bc2806edd0bc1fe7e8a47b0686ae93cbb1a8b41e
teamsoftapps <softapps.io2@gmail.com>
2022-12-06 16:17:45 +0500

    remove log

--- services/api.js
+++ services/api.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-console.log({
-  base_url: process.env.base_url,
-  media_url: process.env.media_url,
-});
-
 const api = axios.create({
   baseURL: process.env.base_url,
 });
